215e083a1d7ed5f55ee7f7f86a5c217f6a4ade3d
Sayeed Ouddin <sl5332s@login.missouristate.edu>
2023-05-25 13:02:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CombineLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
